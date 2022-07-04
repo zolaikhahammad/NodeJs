7fac3eb5d61c5a082adf5a492a9426e91e00e36d
Zolaikha Hammad <msns.sweety@gmail.com>
2022-07-04 09:03:56 +0500

    How express js functions works at the backend
    we can shorten our code by using the express js and define a proper structure by using the app.listen which is the same as we used to create server and res.send() we do not need to add header or write a response ourselves we can send response of any type

--- node-server/server.js
+++ node-server/server.js
@@ -8,7 +8,7 @@
 //     ////anonymous function approach
 // });
 
-const http=require('http');
+//const http=require('http');
 const express=require('express');
 const app=express(); ///// express function express framework, all about middleware
 app.use((req,res,next)=>{
@@ -18,7 +18,9 @@ app.use((req,res,next)=>{
 }); ///to add new middleware function,will be executed for every request, next is a function, has to be excuted to allow the request to travel to the next req
 app.use((req,res,next)=>{
     console.log('in another the middleware');
+    res.send("<h1> HELLO FROM EXPRESS</h1>"); ///allows us to send response of type ay
    
 });
-const server=http.createServer(app);
-server.listen(3000); /////starts a process will keep running to listen incoming request
+app.listen(3000);
+// const server=http.createServer(app);
+// server.listen(3000); /////starts a process will keep running to listen incoming request
