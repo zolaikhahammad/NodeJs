7fe627f1f84e4bb446922317e1ad33e0f9d998f1
Zolaikha Hammad <msns.sweety@gmail.com>
2022-07-23 17:44:51 +0500

    output dynamic content using pug
    we need ti pass the json model intot the templating engine
    template engine are no the part of the server

--- node-server/shop.js
+++ node-server/shop.js
@@ -5,9 +5,11 @@ const rootDir=require('../helpers/path');
 const adminData=require('./admin')
 router.get('/',(req,res,next)=>{
     //res.send("<h1> HELLO FROM EXPRESS</h1>"); ///allows us to send response of type ay
-    console.log(adminData.product);
+    //console.log(adminData.product);
+    const product=adminData.product;
+    res.render('shop',{prods:product});
    //res.sendFile(path.join(rootDir,'../','views','shop.html'));
    ////to send a response for the pug template we use render method
-   res.render('shop'); //user default template engine, and views path is defined in the view engine
+   //res.render('shop'); //user default template engine, and views path is defined in the view engine
 });
 module.exports=router;
